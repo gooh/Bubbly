1e66b7cf68f27ebce672a3b878970366c62fc72a
gooh <info@gordon-oheim.de>
2014-12-29 17:33:45 +0100

    improve layout
    the current layout is still a rough prototype. In order to improve
    it, I am centering the charts, add some colors, make the chart widths
    more dynamic and also fixing the time axis on the bubble chart to
    always have text labels rotated by 90 degree.

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,9 +3,19 @@
         <meta charset="utf-8">
         <title>Test Code Metrics</title>
         <style type="text/css">
+            body {
+                background-color: #eee;
+                color: #111;
+                text-align: center;
+            }
             div {
                 font-family: sans-serif;
                 margin-bottom: 20px;
+            }
+            div svg {
+                background-color: white;
+                border: 1px solid #333;
+                height: 600px;
             }
         </style>
         <script src="/js/jquery.min.js"></script>
@@ -19,7 +29,7 @@
             <h1>Test to Code Ratio per Commit</h1>
         </div>
         <div id="test-ratio-on-average">
-            <h2>Average Test to Code Ratio by Author</h2>
+            <h1>Average Test to Code Ratio by Author</h1>
         </div>
     </body>
 </html>
--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -7,7 +7,7 @@ d3.json('/get_metrics.php' + queryString, function (json) {
         data.push({
             'id': commit.commitHash,
             'date': new Date(commit.date * 1000),
-            'author': commit.author,
+            'author': commit.author.split('<')[0],
             'message': commit.message.match('.+?\n'),
             'ratio': commit.changedFiles.testCodeRatio,
             'size': commit.changedFiles.changes,
@@ -15,21 +15,30 @@ d3.json('/get_metrics.php' + queryString, function (json) {
         });
     });
 
+    var transitionTime = 1000;
+    var margin = {top: 75, right: 75, bottom: 75, left: 75};
+    var width = parseInt(d3.select('#test-ratio-per-commit').style('width'), 10)
+        - margin.left - margin.right;
+
     // configure the bubble chart
     var svg = dimple.newSvg("#test-ratio-per-commit", "95%", "95%");
     var chart = new dimple.chart(svg, data);
-    chart.setBounds(50, 35, 975, 425);
+    chart.setBounds(margin.left, margin.top, width - 250, 425);
     chart.addCategoryAxis("x", ["date"]);
     chart.addMeasureAxis("y", "ratio");
     chart.addLogAxis("z", "size", .1);
     chart.axes[0].timeField = "date";
+    chart.axes[0].title = null;
     chart.axes[0].timePeriod = d3.time.days;
     chart.axes[0].timePeriod.timeInterval = 1;
+    chart.axes[0].tickFormat = '%x';
 
     chart.addSeries(["message", "id", "files", "author"], dimple.plot.bubble);
     // draw legend
-    var myLegend = chart.addLegend("100%,10px", 100, 60, 300, "Left");
-    chart.draw();
+    var myLegend = chart.addLegend(width - 150, 75, 225, 425);
+    chart.draw(transitionTime);
+    chart.axes[0].shapes.selectAll('text').attr('transform', 'translate(14,35)rotate(90)');
+
     // This is a critical step.  By doing this we orphan the legend. This
     // means it will not respond to graph updates.  Without this the legend
     // will redraw when the chart refreshes removing the unchecked item and
@@ -37,9 +46,8 @@ d3.json('/get_metrics.php' + queryString, function (json) {
     chart.legends = [];
     // Get a unique list of Owner values to use when filtering
     var filterValues = dimple.getUniqueValues(data, "author");
-    // Get all the rectangles from our now orphaned legend
-    // Add a click event to each rectangle
-    myLegend.shapes.selectAll("rect").on("click", function (e) {
+    // Add a click event to each author name
+    myLegend.shapes.selectAll("text").on("click", function (e) {
         // This indicates whether the item is already visible or not
         var hide = false;
         var newFilters = [];
@@ -62,7 +70,7 @@ d3.json('/get_metrics.php' + queryString, function (json) {
         filterValues = newFilters;
         // Filter the data
         chart.data = dimple.filterData(data, "author", filterValues);
-        chart.draw(window.innerWidth);
+        chart.draw(transitionTime);
     });
 
     // average the ratios
@@ -87,11 +95,11 @@ d3.json('/get_metrics.php' + queryString, function (json) {
     // configure the bar chart
     var svg2 = dimple.newSvg("#test-ratio-on-average", "95%", "95%");
     var chart2 = new dimple.chart(svg2, data2);
-    chart2.setBounds(50, 35, 975, 425);
+    chart2.setBounds(margin.left, margin.top, width - 75, 425);
     chart2.addCategoryAxis("x", "author");
     chart2.addMeasureAxis("y", "average");
     chart2.axes[1].tickFormat = ',';
     chart2.axes[1].title = 'average ratio';
     chart2.addSeries(["author", "commits", "average"], dimple.plot.bar);
-    chart2.draw();
+    chart2.draw(transitionTime);
 });
